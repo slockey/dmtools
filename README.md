bd5d7ac0707e8029d2a47f983453a5c3aff85278
Steven Lockey <steven.lockey@cision.com>
2020-06-29 22:07:28 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# dmtools
+# README.md
+
+## Requirements
+
+Note: some of the steps below imply use of a Debian Linux variant. Originally, implemented on a modern Ubuntu.
+
+- Python 2.7
+- Docker
+
+## Install Docker
+
+On a Debian variant:
+
+```
+sudo apt-get install docker.io --install-suggests
+```
+
+Verify docker install
+
+```
+sudo docker run --rm -ti ubuntu:latest /bin/bash
+```
+
+### ElasticSearch
+
+Download elasticsearch docker
+
+```
+sudo docker pull docker.elastic.co/elasticsearch/elasticsearch:6.2.3
+```
+
+Start your new docker container
+
+```
+sudo docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" --name elastic docker.elastic.co/elasticsearch/elasticsearch:6.2.3
+```
+
+### Kibana
+
+Download kibana docker
+
+```
+sudo docker pull docker.elastic.co/kibana/kibana:6.2.3
+```
+
+Run kibana docker
+
+```
+sudo docker run --rm --link elastic:elastic-url -e "ELASTICSEARCH_URL=http://elastic-url:9200"  -p 5601:5601 --name kibana docker.elastic.co/kibana/kibana:6.2.3
+```
+
+## Load Data Into ElasticSearch
+
+Remove license from json file
+
+- TBD - notes to do this OR provide script
+
+Index content of json file
+
+```
+cat <5e-SRD-Monsters.json> | jq -c '.[] | {"index": {"_index": "monsters", "_type": "monster", "_id": .name}}, .' | curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/_bulk --data-binary @-
+```
+
---
+++ scripts/merge-json.py
@@ -0,0 +1,63 @@
+
+import sys
+import collections
+import jprops
+import json
+import logging
+import os
+
+from pprint import pprint
+
+logging.basicConfig(format='%(asctime)s %(levelname)s check_semasiodata.py: %(message)s', level=logging.INFO)
+
+# merge 5e monsters with their environment metadata
+
+
+def removeLicense(monsters):
+    logging.info("Removing license object from {}".format(monstersSourceFile))
+    for monster in monsters:
+        if 'license' in monster:
+            del monster['license']
+    return monsters
+
+
+def mergeMetadata(monsters, environments, outFile):
+    for monster in monsters:
+        logging.info(monster['name'])
+        envListToAdd = []
+        for env in environments:
+            for monsterInEnv in env['monsters']:
+                if monster['name'] in monsterInEnv['name']:
+                    envListToAdd.append(env['name'])
+                    break
+        # append the env list to the monster
+        monster['environments'] = envListToAdd
+
+    # create the output file
+    with open(outFile, 'w') as outfile:
+        json.dump(monsters, outfile)
+    
+    logging.info("Completed merge.");
+
+
+# call main
+if __name__ == '__main__':
+
+    # load configuration properties
+    propertyfile = sys.argv[1]
+    
+    if os.path.isfile(propertyfile) == False:
+        raise Exception('Configuration File %s  does not exist.', propertyfile)
+    
+    with open(propertyfile) as F:
+        props = jprops.load_properties(F, collections.OrderedDict)
+        
+        monstersSourceFile = props['monsters.file.path']
+        envMappingSourceFile = props['monsters.by.environment.file.path']
+        mergedOutputFile = props['merged.output.file.path']
+
+    monsters = json.load(open(monstersSourceFile))
+    environments = json.load(open(envMappingSourceFile))
+ 
+    monsters = removeLicense(monsters)
+    mergeMetadata(monsters, environments, mergedOutputFile)
